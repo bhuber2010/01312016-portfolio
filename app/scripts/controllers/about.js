981b5ed67f1777da67865ccf31deafaffd7d09bb
bhuber2010 <bhuber@alumni.colostate.edu>
2016-01-31 13:36:15 -0700

    scaffold

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bhuberApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bhuberApp
+ */
+angular.module('bhuberApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
